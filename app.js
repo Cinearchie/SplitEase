e451e1d26dae6fbecf4f34835416c62a36dc0402
Archishman Adhikari <102647629+Cinearchie@users.noreply.github.com>
2025-02-15 21:55:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -81,5 +81,5 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
