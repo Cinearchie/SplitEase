d7967eca49f3c27bfea9d3528d424887b2b3db0d
Archishman Adhikari <archishmana@gmail.com>
2025-03-07 18:15:33 +0530

    Removed some parts of code again 2

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+app.use(cors());
 
 
 // Session configuration (required for flash messages)
